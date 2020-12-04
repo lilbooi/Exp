0a5ed1934734282aab1759ae4dc657c9eb94e9ed
xuwanwei1205@163.com <xuwanwei1205@163.com>
2020-12-04 19:03:55 +0800

    add

---
+++ exp2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"xuwanwei 18CS1 201830582128"<<endl;
+    char progName[105];
+    while(scanf("%s",progName)!=EOF){
+        
+    }
+
+}
